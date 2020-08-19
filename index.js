2565392a38cc1a2f3faa9e0e678b45e8c95faabc
MARIANNACROCHA <marianna.camillo@gmail.com>
2020-08-19 04:27:08 -0300

    ajustes

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ $( document ).ready(function() {
     $("#form").hide();
     $("#divFim").hide();
     $("#topic1").hide();
+    $("#topic4").hide();
+    $("#topic5").hide();
+    $("#topic6").hide();
+    $("#topic7").hide();
+   
  
 
     $("#new-topic").click(function(){
@@ -16,12 +21,22 @@ $( document ).ready(function() {
         $("#form").hide();
         $("#divFim").show();
         $("#topic1").show();
-      
-
     });
 
-   
-    
+    $("#new-topic2").click(function(){
+        $("#divTopo").hide();
+        $("#form").show();
+        $("#divFim").hide();
+        $("#topic1").hide();
+    });
+
+    $("#topic3").click(function() {
+        $("#topic4").show();
+        $("#topic5").show();
+        $("#topic6").show();
+        $("#topic7").show();
+    })
+
 });
 
 
